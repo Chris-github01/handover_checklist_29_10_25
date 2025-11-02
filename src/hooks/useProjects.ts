f48896b5adf88c038a3483628065be3b05a65086
Chris-github01 <90413281+Chris-github01@users.noreply.github.com>
2025-11-03 08:48:35 +1300

    Updated useProjects.ts

--- src/hooks/useProjects.ts
+++ src/hooks/useProjects.ts
@@ -19,7 +19,7 @@ export const useProjects = () => {
       
       const recipients = [
         'pieter@optimalfire.co.nz',
-        'pedro@optimalfire.co.nz'
+        'ray@optimalfire.co.nz'
       ];
       
       const emailSubject = `New Project Created: ${project.name}`;

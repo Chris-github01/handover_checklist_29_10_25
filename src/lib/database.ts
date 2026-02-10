5f80b6dbb7601d577b1d8300a24fbed3b332dcfc
Chris-github01 <90413281+Chris-github01@users.noreply.github.com>
2026-02-10 13:04:32 +1300

    Updated database.ts

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -473,7 +473,7 @@ export const initializeProjectStages = async (projectId: string, projectStatus?:
     },
     {
       code: 'STEP_8',
-      title: 'Step 8: Site Managers (Ali/Alfie/Zach/Chris/Karel/Stuart)',
+      title: 'Step 8: Site Managers',
       owner_role: 'PM/SM',
       order_index: 8,
       is_required: true,
